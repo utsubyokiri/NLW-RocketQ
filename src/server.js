1e79d89759244c58aca63bebc639c7ac406325d2
Everton Araujo <etuetuetuetu25@gmail.com>
2021-08-26 08:41:30 -0300

    Modal fixes

--- src/server.js
+++ src/server.js
@@ -6,9 +6,10 @@ const server = express()
 
 server.set('view engine', 'ejs')
 
+server.use(express.static('public'))
+
 server.set('views', path.join(__dirname, 'views'))
 
 server.use(route)
 
-server.listen(3000, () => console.log("RODANDO"))
-
+server.listen(3000, () => console.log('RODANDO'))
